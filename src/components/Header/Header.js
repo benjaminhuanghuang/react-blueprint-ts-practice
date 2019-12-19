3363352d8926b3e429c08cfd94dec33672548a65
Benjamin Huang <bhuang@rms.com>
2019-12-19 11:40:18 -0800

    fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,73 +1,77 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { NavLink } from 'react-router-dom';
 import { connect } from "react-redux";
 //
 import './Header.scss';
 
 class Header extends Component {
-
-  renderAuthButton(){
+  renderAuthButton() {
     if (this.props.authenticated) {
-      return  (
+      return (
         <li className="nav-item">
           <NavLink exact activeClassName="activeNav" to="/signout">sign out</NavLink>
         </li>
       );
     }
-    else{
+    else {
       return [
         <li className="nav-item" key={1}>
-        <NavLink exact activeClassName="activeNav" to="/signin">sign in</NavLink>
-      </li>,
-      <li className="nav-item" key={2}>
-        <NavLink exact activeClassName="activeNav" to="/signup">sign up</NavLink>
-      </li>
+          <NavLink exact activeClassName="activeNav" to="/signin">sign in</NavLink>
+        </li>,
+        <li className="nav-item" key={2}>
+          <NavLink exact activeClassName="activeNav" to="/signup">sign up</NavLink>
+        </li>
       ];
     }
   }
 
   render() {
     return (
-        <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <NavLink className="navbar-brand" to="/"> React+Redux</NavLink>
-            </div>
-            <div className="collapse navbar-collapse">
-              <ul className = "nav navbar-nav">
-                <li className="nav-item">
-                  <NavLink to="/" exact activeClassName ="active">Home</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/users" activeClassName ="active">Users</NavLink>
-                </li>
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button">
-                    Options <span className="caret"></span>
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" className="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li role="separator" className="divider"></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
-               </ul>
-              <ul className="nav navbar-nav navbar-right">
-                {this.renderAuthButton()}
-              </ul>
-            </div>
-          </div> {/*container-fluid*/}
-        </nav>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <NavLink className="navbar-brand" to="/"> React+Redux</NavLink>
+          </div>
+          <div className="collapse navbar-collapse">
+            <ul className="nav navbar-nav">
+              <li className="nav-item">
+                <NavLink to="/" exact activeClassName="active">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/users" activeClassName="active">Users</NavLink>
+              </li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button">
+                  Options <span className="caret"></span>
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" className="divider"></li>
+                  <li><a href="#">Separated link</a></li>
+                  <li role="separator" className="divider"></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              {this.renderAuthButton()}
+            </ul>
+          </div>
+        </div> {/*container-fluid*/}
+      </nav>
     );
   }
 }
-export default Header;
-// function mapStateToProps(state) {
-//   return { authenticated: state.authenticated };
-// }
 
-// export default connect(mapStateToProps, actions)(Header);
+Header.propTypes = {
+  authenticated: PropTypes.bool.isRequired
+}
+
+function mapStateToProps(state) {
+  return { authenticated: state.auth.isAuthed };
+}
+
+export default connect(mapStateToProps)(Header);
